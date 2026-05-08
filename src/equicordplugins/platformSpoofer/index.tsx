91ceac2c3dd0d82960dabde2bfe8069561b19899
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-05-08 15:14:13 -0400

    PlatformSpoofer: Spoof Experiment again

--- src/equicordplugins/platformSpoofer/index.tsx
+++ src/equicordplugins/platformSpoofer/index.tsx
@@ -75,12 +75,18 @@ export default definePlugin({
             ]
         },
         {
-            find: ".ORBS,label:",
-            replacement: {
-                match: /\{enabled:(\i).{0,25}"collectibles_shop_header_bar"\}\)/,
-                replace: "$1=true"
-            }
-        }
+            find: '"2025-01-virtual-currency-rollout"',
+            replacement: [
+                {
+                    match: /(?<=\}\),)(\i)/,
+                    replace: "$1=e=>({enabled:true}),_equicord_$1"
+                },
+                {
+                    match: /(?<=\.getConfig\(\i\)\.enabled\},)(\i)/,
+                    replace: "$1=e=>({enabled:true}),_equicord_$1"
+                }
+            ]
+        },
     ],
     getPlatform(bypass, userId?: any) {
         const platform = settings.store.platform ?? "desktop";
